c8bc3fb637cf4089f09b0b4306704c8b3c46d9f5
Roka0430 <noxx0430@gmail.com>
2026-09-22 15:39:46 +0900

    feat: ログイン機能を実装

--- modules/routes.py
+++ modules/routes.py
@@ -1,15 +1,57 @@
-from flask import Flask, render_template, jsonify
-from flask_login import LoginManager, UserMixin, login_user, login_required
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    current_user,
+    login_required,
+)
+from werkzeug.security import check_password_hash
+
+
+class Admin(UserMixin):
+    id = "admin"
+
 
 app = Flask(__name__, template_folder="../templates")
+app.secret_key = "secret_key"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login_get"
 
 
-@app.route("/login")
-def login():
+@login_manager.user_loader
+def load_user(user_id) -> Admin | None:
+    if user_id == "admin":
+        return Admin()
+    return None
+
+
+@app.get("/login")
+def login_get():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
     return render_template("login.html")
 
 
-@app.route("/")
+@app.post("/login")
+def login_post():
+    if current_user.is_authenticated:
+        return redirect(url_for("routes.home"))
+
+    password = request.form["password"]
+
+    if check_password_hash("password_hash", password):
+        login_user(Admin())
+        return redirect(url_for("routes.home"))
+
+    return render_template("login.html", error="パスワードが正しくありません")
+
+
+@app.get("/")
+@login_required
 def home():
     return render_template("index.html")
 
