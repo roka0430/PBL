c367765a5382a3e38f62bbd204c716eb4a83f11d
Roka0430 <noxx0430@gmail.com>
2026-09-23 09:48:21 +0900

    feat: ビューアーパスワードの入力機能を追加し、環境変数に対応

--- tools/generate_seacret.py
+++ tools/generate_seacret.py
@@ -3,16 +3,32 @@ from getpass import getpass
 from werkzeug.security import generate_password_hash
 
 while True:
-    password = getpass("Admin password: ")
+    print("<Admin>")
+    admin_password = getpass("Input password: ")
     confirm = getpass("Confirm password: ")
 
-    if password == confirm:
+    if admin_password == confirm:
         break
 
-    print("Passwords do not match. Try again.")
+    print("\033[31mPasswords do not match. Try again.\033[0m\n")
+
+print("")
+
+while True:
+    print("<Viewer>")
+    viewer_password = getpass("Input password: ")
+    confirm = getpass("Confirm password: ")
+
+    if viewer_password == confirm:
+        break
+
+    print("\033[31mPasswords do not match. Try again.\033[0m\n")
 
 secret_key = secrets.token_hex(32)
-password_hash = generate_password_hash(password)
+admin_password_hash = generate_password_hash(admin_password)
+viewer_password_hash = generate_password_hash(viewer_password)
 
-print("\nSECRET_KEY=" + secret_key)
-print("ADMIN_PASSWORD_HASH=" + password_hash)
+print("")
+print("\033[31mSECRET_KEY=\033[0m" + secret_key)
+print("\033[31mADMIN_PASSWORD_HASH=\033[0m" + admin_password_hash)
+print("\033[31mVIEWER_PASSWORD_HASH=\033[0m" + viewer_password_hash)
