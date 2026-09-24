dcaf9219dfffc7d2aa3fee2649edc25f04cf46c5
Roka0430 <noxx0430@gmail.com>
2026-09-24 21:52:32 +0900

    feat: Alpineストアにユーザーのroleを管理する機能を追加

--- static/js/common.js
+++ static/js/common.js
@@ -1,5 +1,22 @@
 import theme from "./modules/theme.js";
 
 document.addEventListener("alpine:init", () => {
+  Alpine.store("role", {
+    role: "",
+
+    get label() {
+      return (
+        {
+          admin: "管理者",
+          viewer: "閲覧者",
+        }[this.role] ?? ""
+      );
+    },
+
+    init() {
+      this.role = document.body?.dataset.userRole ?? "";
+    },
+  });
+
   Alpine.data("common_theme", () => theme);
 });
