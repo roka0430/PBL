7a74a778e6d9b6a18031ca1073a6cb6d266e0aa3
Roka0430 <noxx0430@gmail.com>
2026-09-25 12:18:07 +0900

    feat: WateringControllerにコマンドキューを追加し、給水命令を処理する機能を実装

--- modules/watering.py
+++ modules/watering.py
@@ -1,11 +1,65 @@
 import time
+import queue
+from enum import Enum
+from dataclasses import dataclass
+
+SENSOR_CHECK_INTERVAL_SEC = 180  # 土壌水分量を測定する間隔(秒)
+
+
+# ------------------------------ Status ------------------------------
+
+
+class WateringStatus(Enum):
+    IDLE = "idle"  # 待機中
+    WATERING = "watering"  # 給水中
+    CALIBRATING = "calibrating"  # 校正中
+    ERROR = "error"  # エラー
+
+
+# ------------------------------ Command ------------------------------
+
+
+class WateringCommandType(Enum):
+    WATERING = "watering"  # 給水命令
+
+
+@dataclass
+class WateringCommand:
+    type: WateringCommandType
+    amount_ml: int
+
+
+# ------------------------------ Controller ------------------------------
 
 
 class WateringController:
+    def __init__(self):
+        self.status = WateringStatus.IDLE
+        self.command_queue = queue.Queue()
+
     def mainloop(self):
+
+        next_sensor_check = time.monotonic()
+
         while True:
-            print("watering mainloop")
-            time.sleep(5)
+            try:
+                command = self.command_queue.get(
+                    timeout=max(0, next_sensor_check - time.monotonic())
+                )
+                self._process_command(command)
+
+            except queue.Empty:
+                self._check_sensor()
+                next_sensor_check = time.monotonic() + SENSOR_CHECK_INTERVAL_SEC
+
+    def _process_command(self, command):
+        print("-----------------------------")
+        print("command:", command.type.value)
+        print("amount:", command.amount_ml)
+        print("-----------------------------")
+
+    def _check_sensor(self):
+        print("check sensor")
 
     def request_watering(self, amount_ml):
         try:
@@ -16,7 +70,8 @@ class WateringController:
         if not 10 <= amount_ml <= 500:
             return False
 
-        print("request:", amount_ml)
-        # ここでqueue put
+        self.command_queue.put(
+            WateringCommand(type=WateringCommandType.WATERING, amount_ml=amount_ml)
+        )
 
         return True
